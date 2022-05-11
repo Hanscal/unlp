9037d91acc948eec5ba8c018fe6eb5ebf1ed1cb1
ilovesxfx <105336191+ilovesxfx@users.noreply.github.com>
2022-05-11 15:33:00 +0800

    Update synonym_replace.py

--- unlp/augment/eda/synonym_replace.py
+++ unlp/augment/eda/synonym_replace.py
@@ -1,85 +1,75 @@
 # -*- coding: utf-8 -*-
 
 """
-@Time    : 2022/3/2 10:32 上午
+@Time    : 2022/3/1 9:35 上午
 @Author  : hcai
 @Email   : hua.cai@unidt.com
 """
+# 回译
 
-import os
-import numpy as np
-import jieba
-import jieba.analyse
-from gensim.models import KeyedVectors
-from gensim import matutils
+from urllib.parse import quote
+import http.client
+import hashlib
+import urllib
+import random
+import json
+import time
 
 
-def read_samples(file_path):
-    samples = []
-    with open(file_path, 'r', encoding='utf8') as file:
-        for line in file:
-            if not line.strip():
-                continue
-            samples.append(line.strip())
-    return samples
-
-class EmbedReplace(object):
-    def __init__(self, wv_path, **kwargs):
-        binary = False
-        if wv_path.endswith('bin'):
-            binary = True
-        print("loading word2vec model from {}".format(wv_path))
-        self.wv = KeyedVectors.load_word2vec_format(wv_path, binary=binary)
-
-    def is_chinese(self, word):
-        """是否为中文字符
-        :param word:
-        :return:
+class Translator(object):
+    def translate(self, q, src_lang, tgt_lang):
         """
-        for ch in word:
-            if '\u4e00' <= ch <= '\u9fff':
-                return True
-        return False
-
-    def vectorize(self, docs, vocab_size):
-        return matutils.corpus2dense(docs, vocab_size)
-
-    def extract_keywords(self, text, topk=5):
-        """ 提取关键词
-        :param topk: 前 topk 个关键词
-        :return: 返回的关键词列表
+        :param q:翻译前文本
+        :param src_lang: 原始语言
+        :param tgt_lang: 目标语言
+        :return: 翻译后的文本
+        请求百度通用翻译API，详细请看 https://api.fanyi.baidu.com/doc/21
         """
-        keys = jieba.analyse.extract_tags(text, topK=topk)
+        appid = '20220302001105912'  # Fill in your AppID
+        secretKey = 'Gpginf0ryq4tRXBEgGyD'  # Fill in your key
 
-        return keys
+        httpClient = None
+        myurl = '/api/trans/vip/translate'
 
-    def replace_words(self, sample, keywords, ratio=0.2):
-        """用wordvector的近义词来替换，并避开关键词
-        :param sample (list): reference token list
-        :param keywords (list): A reference represented by a word bag model
-        :return: 新的文本
+        salt = random.randint(0, 4000)
+        sign = appid + q + str(salt) + secretKey
+        sign = hashlib.md5(sign.encode()).hexdigest()
+        myurl = '/api/trans/vip/translate' + '?appid=' + appid + '&q=' + urllib.parse.quote(
+            q) + '&from=' + src_lang + '&to=' + tgt_lang + '&salt=' + str(salt) + '&sign=' + sign
+
+        try:
+            httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
+            httpClient.request('GET', myurl)
+            # response is HTTPResponse object
+            response = httpClient.getresponse()
+            result_all = response.read().decode("utf-8")
+            result = json.loads(result_all)
+
+            return result
+
+        except Exception as e:
+            print(e)
+        finally:
+            if httpClient:
+                httpClient.close()
+
+    def back_translate(self, text, src_lang="zh", tgt_lang="en"):
         """
-        #
-        num = int(len(sample) * ratio) # 随机替换30%的单词
-        new_tokens = sample.copy()
-        indexes = np.random.choice(len(sample), num)
-        for index in indexes:
-            token = sample[index]
-            if self.is_chinese(token) and token not in keywords and token in self.wv:
-                new_tokens[index] = self.wv.most_similar(positive=token, negative=None, topn=1)[0][0]
+        :param text: 文本
+        :param src_lang: 原始语言
+        :param tgt_lang: 目前语言
+        :return: 回译后的文本
+        """
+        en = self.translate(text, src_lang, tgt_lang)['trans_result'][0]['dst']
+        time.sleep(1.5)
+        target = self.translate(en, tgt_lang, src_lang)['trans_result'][0]['dst']
+        time.sleep(1.5)
 
-        return new_tokens
+        return en, target
 
-    def run_replace(self, sample):
-        keys = self.extract_keywords(sample)
-        tokens = jieba.lcut(sample)
-        res = self.replace_words(tokens, keys)
-        return res
 
 if __name__ == '__main__':
-    data_dir = '/Volumes/work/project/unlp/unlp/augment/data'
-    samples = read_samples(os.path.join(data_dir, 'corpus.txt'))
-    wv_path = '/Volumes/work/project/unlp//unlp/transformers/word2vec/light_Tencent_AILab_ChineseEmbedding.bin'
-    replacer = EmbedReplace(wv_path)
-    res = replacer.run_replace(samples)
-    print(res)
+    t = Translator()
+    en, tgt = t.back_translate('你好呀', src_lang='zh', tgt_lang='en')
+    print("English", en)
+    print("Chinese", tgt)
