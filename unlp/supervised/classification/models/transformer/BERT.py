ac3aa7ba3fef8537503d4b2c7e145f096b8bbb83
hanscal <hanscalcai@163.com>
2022-09-15 17:57:34 +0800

    添加log路径

--- unlp/supervised/classification/models/transformer/BERT.py
+++ unlp/supervised/classification/models/transformer/BERT.py
@@ -42,13 +42,14 @@ class Config(object):
         self.train_path = dataset + '/data/train.txt'                                # 训练集
         self.dev_path = dataset + '/data/dev.txt'                                    # 验证集
         self.test_path = dataset + '/data/test.txt'                                  # 测试集
+        self.log_path = dataset + '/log/' + self.model_name
         self.class_list = [x.strip() for x in open(dataset + '/data/class.txt').readlines()] # 类别名单
         self.save_path = dataset + '/saved_dict/' + self.model_name                  # 模型训练结果
         if not os.path.isdir(self.save_path):
             os.makedirs(self.save_path)
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
 
-        self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
+        self.require_improvement = 2000                                 # 若超过2000batch效果还没提升，则提前结束训练
         self.num_classes = len(self.class_list)                         # 类别数
         self.num_epochs = 3                                             # epoch数
         self.batch_size = 128                                           # mini-batch大小128
