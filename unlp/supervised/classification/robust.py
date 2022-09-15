fdcd52447d0fbb56eebea75befaf681645456fcd
hanscal <hanscalcai@163.com>
2022-09-15 17:56:49 +0800

    添加分类泛化模块

---
+++ unlp/supervised/classification/robust.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2022/9/14 10:50 上午
+@Author  : hcai
+@Email   : hua.cai@unidt.com
+"""
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BinaryFocalLoss(nn.Module):
+    """
+    Focal_Loss= -1*alpha*(1-pt)*log(pt)
+    :param alpha: (tensor) 3D or 4D the scalar factor for this criterion
+    :param gamma: (float,double) gamma > 0 reduces the relative loss for well-classified examples (p>0.5) putting more
+                    focus on hard misclassified example
+    :param reduction: `none`|`mean`|`sum`
+    """
+
+    def __init__(self, alpha=1, gamma=2, reduction='mean', **kwargs):
+        super(BinaryFocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.smooth = 1e-6  # set '1e-4' when train with FP16
+        self.reduction = reduction
+
+        assert self.reduction in ['none', 'mean', 'sum']
+
+    def forward(self, output, target):
+        prob = torch.sigmoid(output)
+        prob = torch.clamp(prob, self.smooth, 1.0 - self.smooth)
+
+        target = target.unsqueeze(dim=1)
+        pos_mask = (target == 1).float()
+        neg_mask = (target == 0).float()
+
+        pos_weight = (pos_mask * torch.pow(1 - prob, self.gamma)).detach()
+        pos_loss = -pos_weight * torch.log(prob)  # / (torch.sum(pos_weight) + 1e-4)
+
+        neg_weight = (neg_mask * torch.pow(prob, self.gamma)).detach()
+        neg_loss = -self.alpha * neg_weight * F.logsigmoid(-output)  # / (torch.sum(neg_weight) + 1e-4)
+
+        loss = pos_loss + neg_loss
+        loss = loss.mean()
+        return loss
+
+
+class MultiFocalLoss(nn.Module):
+    """
+    Focal_Loss= -1*alpha*((1-pt)**gamma)*log(pt)
+    Args:
+        num_class: number of classes
+        alpha: class balance factor shape=[num_class, ]
+        gamma: hyper-parameter
+        reduction: reduction type
+    """
+
+    def __init__(self, gamma=2, alpha=None, reduction='mean'):
+        super(MultiFocalLoss, self).__init__()
+        self.gamma = gamma
+        self.reduction = reduction
+        self.alpha = alpha
+        self.smooth = 1e-4
+
+
+    def forward(self, logit, target, num_class):
+        # assert isinstance(self.alpha,torch.Tensor)\
+        if self.alpha is None:
+            self.alpha = torch.ones(num_class, ) - 0.5
+        elif isinstance(self.alpha, (int, float)):
+            self.alpha = torch.as_tensor([self.alpha] * num_class)
+        elif isinstance(self.alpha, (list, np.ndarray)):
+            self.alpha = torch.as_tensor(self.alpha)
+        if self.alpha.shape[0] != num_class:
+            raise RuntimeError('the length not equal to number of class')
+        alpha = self.alpha.to(logit.device)
+        prob = F.softmax(logit, dim=1)
+
+        if prob.dim() > 2:
+            # used for 3d-conv:  N,C,d1,d2 -> N,C,m (m=d1*d2*...)
+            N, C = logit.shape[:2]
+            prob = prob.view(N, C, -1)
+            prob = prob.transpose(1, 2).contiguous()  # [N,C,d1*d2..] -> [N,d1*d2..,C]
+            prob = prob.view(-1, prob.size(-1))  # [N,d1*d2..,C]-> [N*d1*d2..,C]
+
+        ori_shp = target.shape
+        target = target.view(-1, 1)
+
+        prob = prob.gather(1, target).view(-1) + self.smooth  # avoid nan
+        logpt = torch.log(prob)
+        # alpha_class = alpha.gather(0, target.squeeze(-1))
+        alpha_weight = alpha[target.squeeze().long()]
+        loss = -alpha_weight * torch.pow(torch.sub(1.0, prob), self.gamma) * logpt
+
+        if self.reduction == 'mean':
+            loss = loss.mean()
+        elif self.reduction == 'none':
+            loss = loss.view(ori_shp)
+
+        return loss
+
+# 嵌入层对抗训练，增强模型鲁棒性
+class FGM():
+    def __init__(self, model):
+        self.model = model
+        self.backup = {}
+
+    def attack(self, epsilon=1., emb_name='emb'): # 修改Embedding
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and emb_name in name:
+                self.backup[name] = param.data.clone()
+                norm = torch.norm(param.grad)
+                if norm != 0:
+                    r_at = epsilon * param.grad / norm
+                    param.data.add_(r_at)
+
+    def restore(self, emb_name='emb'):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and emb_name in name:
+                assert name in self.backup
+                param.data = self.backup[name]
+        self.backup = {}
